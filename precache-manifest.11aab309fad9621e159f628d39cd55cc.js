50656efab9b8104659aa8d819b22da6e316e9fee
shivdeep1 <shivdeepsachdeva@gmail.com>
2020-05-29 02:01:21 +0530

    Updates

---
+++ precache-manifest.11aab309fad9621e159f628d39cd55cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d57eaefa33fa815ff4b6e9f5dbfce3",
+    "url": "/Weddingplanner/index.html"
+  },
+  {
+    "revision": "2c3a35a81ac1ed09b812",
+    "url": "/Weddingplanner/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "aecabec23a0d8e43c03f",
+    "url": "/Weddingplanner/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2c3a35a81ac1ed09b812",
+    "url": "/Weddingplanner/static/js/2.43b8afaa.chunk.js"
+  },
+  {
+    "revision": "d1343243b13320cf3fc3d044c71034f4",
+    "url": "/Weddingplanner/static/js/2.43b8afaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aecabec23a0d8e43c03f",
+    "url": "/Weddingplanner/static/js/main.a712795b.chunk.js"
+  },
+  {
+    "revision": "25a552b483c458b4dd5c",
+    "url": "/Weddingplanner/static/js/runtime-main.021c5e2d.js"
+  }
+]);
